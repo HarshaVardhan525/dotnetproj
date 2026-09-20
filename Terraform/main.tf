e8c4d2b827d6c3b5d2a2894e5da9db31cae28166
HarshaVardhan525 <harshavardhangangumolu@gmail.com>
2026-09-20 10:46:29 +0530

    updated the main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,10 +1,17 @@
 terraform {
-    required_providers {
-      azurerm = {
-        source = "hashicorp/azurerm"
-        version = "~> 4.0"
-      }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
     }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tfbackend"
+    storage_account_name = "quickcartsa"
+    container_name       = "qcterraform"
+    key                  = "quickcart.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
